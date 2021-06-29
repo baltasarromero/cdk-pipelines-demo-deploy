b0f75a4dde0ba985e5c2a7236b7b1362aa92e2eb
baltasarromero <baltasarromero@gmail.com>
2021-06-29 17:11:09 -0300

    Fixed test

--- test/cdk-pipelines-demo.test.ts
+++ test/cdk-pipelines-demo.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as CdkPipelinesDemo from '../lib/cdk-pipelines-demo-stack';
+import * as CdkpipelinesDemoStack from '../lib/cdk-pipelines-demo-stack';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new CdkPipelinesDemo.CdkPipelinesDemoStack(app, 'MyTestStack');
+    const stack = new CdkpipelinesDemoStack.CdkpipelinesDemoStack(app, 'MyTestStack');
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
